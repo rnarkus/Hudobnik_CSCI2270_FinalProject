50ab8192917df656edd21ff7794fc7a66d61daeb
rnarkus <markus_hudobnik@hotmail.com>
2015-04-20 21:43:28 -0600

    added menu options and adjusted delay time

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -6,15 +6,16 @@
 using namespace std;
 
 void displayLogo();
+void displayOptions(Prog&);
 
 int main(){
 	Prog resp = Prog(); 
-	string in_keyword, in_response, input, name;
+	string input, name;
 	displayLogo();
 	resp.insertResp("greet", "Welcome, what is your name?");
 	resp.insertResp("greet", "Welcome, what the cluck is your name?");
-	resp.insertResp("greet", "Hello, my name is Bob, what is your name?");
-	resp.insertResp("greet", "My name is Bob, What can I do for you today?");
+	resp.insertResp("greet", "Hello, my name is BOB, what is your name?");
+	resp.insertResp("greet", "My name is BOB, What can I do for you today?");
 
 	resp.displayResp("greet");
 	cin >> name;
@@ -23,19 +24,39 @@ int main(){
 	resp.insertResp("name", "Guten Tag, " + name +"! Wie geht's?");
 	resp.displayResp("name");
 	
-	while (in_keyword != "quit"){
+	while (input != "quit"){
 		cin >> input;
 			if (input == "1"){
-			cin.ignore();
-			getline(cin, in_keyword);
-			getline(cin, in_response);
-			resp.insertResp(in_keyword, in_response);
-			resp.printAllResps();
+			displayOptions(resp);
 		
 	}
 		
 	}
 	cout << "Goodbye!" << endl;
+}
+
+void displayOptions(Prog& resp){
+	string input2, in_keyword, in_response;
+
+	while(input2 != "3"){
+	cout << "=====Options:=====" << endl;
+	cout << "1. Enter in extra responses for BOB to learn" << endl;
+	cout << "2. Print all Keywords and their responses" << endl;
+	cout << "3. Quit and return to program" << endl;
+		cin >> input2;
+		if (input2 == "1"){
+			cin.ignore();
+			cout << "(Each keyword can have multiple responses)" << endl;
+			cout << "Type in keyword:" << endl;
+			getline(cin, in_keyword);
+			cout << "Type in a response:" << endl;
+			getline(cin, in_response);
+			resp.insertResp(in_keyword, in_response);
+		}
+		else if (input2 == "2"){
+			resp.printAllResps();
+		}
+	}
 }
 
 void displayLogo(){
@@ -53,4 +74,10 @@ void displayLogo(){
 	cout<<"	   B      B  O      O   B      B	   "<<endl;
 	usleep(DELAY);
 	cout<<"	   BBBBBBBB  OOOOOOOO   BBBBBBBB	   "<<endl;
+	cout << endl;
+	cout <<"Coded by: Dieter Markus Hudobnik	   "<<endl;
+	cout <<"Class: CSCI_2270	   "<< endl;
+	cout << endl;
+	cout <<"(type in '1' after greeting to access options menu)	   " << endl;
+	cout << endl;
 }
