73d075c18ed8438ee5e75b3c79e8ec4bdc77178c
rnarkus <markus_hudobnik@hotmail.com>
2015-04-20 19:51:29 -0600

    added initial files

---
+++ Project/Class.h
@@ -0,0 +1,38 @@
+#ifndef PROG_H
+#define PROG_H
+#include <iostream>
+#include <vector>
+
+struct LizaKey
+{
+    std::vector<std::string> response;
+    std::string keyword;
+    LizaKey *next;
+
+    LizaKey(){};
+
+    LizaKey(std::string in_keyword, std::string in_reponse){
+        response.push_back(in_reponse);
+        keyword = in_keyword;
+    }
+};
+
+
+class Prog{
+public:
+    Prog();
+    ~Prog();
+    int hashSum(std::string);
+    void insertResp(std::string, std::string);
+    void insertName(std::string);
+    void findResp(std::string);
+    void printResp();
+    int randNum(int);
+    void printAllResps();
+protected:
+private:
+    LizaKey **hashResp;
+    int ARRAY_SIZE;
+};
+
+#endif // PROG_H
