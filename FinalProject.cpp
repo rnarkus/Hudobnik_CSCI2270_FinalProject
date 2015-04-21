b18aa1d58ada1f85b4ab5b998ec69586fb562313
rnarkus <markus_hudobnik@hotmail.com>
2015-04-20 21:10:46 -0600

    Added displayLogo and the library <unistd.h> to use a delay

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,30 +1,56 @@
 
 #include "Class.h"
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
-void displayWelcome();
+void displayLogo();
 
 int main(){
 	Prog resp = Prog(); 
-	string in_keyword, in_response, input;
+	string in_keyword, in_response, input, name;
+	displayLogo();
+	resp.insertResp("greet", "Welcome, what is your name?");
+	resp.insertResp("greet", "Welcome, what the cluck is your name?");
+	resp.insertResp("greet", "Hello, my name is Bob, what is your name?");
+	resp.insertResp("greet", "My name is Bob, What can I do for you today?");
 
-	displayWelcome();
+	resp.displayResp("greet");
+	cin >> name;
+	resp.insertResp("name", "Hi, " + name +"! How are you today?");
+	resp.insertResp("name", "Hello, " + name +"! What's up?");
+	resp.insertResp("name", "Guten Tag, " + name +"! Wie geht's?");
+	resp.displayResp("name");
+	
 	while (in_keyword != "quit"){
 		cin >> input;
-		if (input == "1"){
-		cin.ignore();
-		getline(cin, in_keyword);
-		getline(cin, in_response);
-		resp.insertResp(in_keyword, in_response);
-		resp.printAllResps();
+			if (input == "1"){
+			cin.ignore();
+			getline(cin, in_keyword);
+			getline(cin, in_response);
+			resp.insertResp(in_keyword, in_response);
+			resp.printAllResps();
+		
 	}
 		
 	}
 	cout << "Goodbye!" << endl;
 }
 
-void displayWelcome(){
-	cout << "welcome" << endl;
+void displayLogo(){
+	int DELAY = 300000;
+	cout<<"	   BBBBBBB   OOOOOOOO   BBBBBBB	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   B     B   O      O   B     B	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   B     B   O      O   B     B	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   BBBBBBB   O      O   BBBBBBB 	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   B      B  O      O   B      B	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   B      B  O      O   B      B	   "<<endl;
+	usleep(DELAY);
+	cout<<"	   BBBBBBBB  OOOOOOOO   BBBBBBBB	   "<<endl;
 }
--- README.txt
+++ README.txt
@@ -6,6 +6,7 @@ N/A
 
 Dependencies
 #include <time.h> // rand() isn’t actually random, you need to set a time using this library to set how long it adds numbers to rand() then it picks from that, is you just use rand() without the library is defaults to 1
+#include <unistd.h> to use usleep();
 
 System Requirements
 Tested and coded on Mac with C++11 pre installed and Command Line Tools 6.2 installed (version 6.3 has bugs, I will update this if 6.3 is fixed). Using SublimeText2 for my IDE.
